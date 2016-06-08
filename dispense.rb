3aeed204ecc63587da43d0e2ad6a951c4917fb50
voodoologic <headley.douglas@gmail.com>
2016-06-08 16:26:05 -0700

    update

--- dispense.rb
+++ dispense.rb
@@ -9,6 +9,7 @@ ruby_version = [Pathname.new(File.absolute_path(__FILE__)).dirname , '.ruby-vers
 ruby_gemset  = [Pathname.new(File.absolute_path(__FILE__)).dirname , '.ruby-gemset'].join('/')
 work_folder  = File.absolute_path(Pathname.new('..'))
 gem_files    = [gemfile, gemfile_lock, ruby_version, ruby_gemset]
+skip_folders = ['global_utilities', 'rubyzoho']
 def link_gem_files(gem_files, entity)
   gem_files.each do |file|
     proposed_file = [entity , Pathname.new(file).basename].join('/')
@@ -19,7 +20,7 @@ def link_gem_files(gem_files, entity)
 end
 
 Dir.glob(work_folder + '/*').each do |entity|
-  if Pathname(entity).directory? && !Pathname(entity).to_s.include?('global_utilities')
+  if Pathname(entity).directory? && !Pathname(entity).to_s.include?(skip_folders)
     link_gem_files(gem_files, entity)
   end
 end
--- global_utilities.rb
+++ global_utilities.rb
@@ -1,16 +1,37 @@
 require 'json'
 module GlobalUtilities
   @@id_mapping = JSON.parse(File.open(File.join(File.dirname(__FILE__), 'id_json.json')).read)
-  module  DB
-    require_relative '../db_share/db'
-  end
-  module Gmail
-    require_relative '../gmail_tool/gmail_tool'
-  end
+
   module Creds
     require_relative '../cred_service/cred_service'
   end
+
+  module ZohoSushi
+    require_relative '../sushi/lib/zoho_sushi'
+  end
+
+  module  DB
+    require_relative '../db_share/db'
+  end
+
+  module Gmail
+    require_relative '../gmail_tool/gmail_tool'
+  end
+
+  module SalesForce
+    require_relative './global_utilities/sales_force'
+  end
+
+  module Zoho
+    require_relative './global_utilities/zoho'
+  end
+
   def self.id_mapping
     @@id_mapping
+  end
+
+  def self.class_from_id(id)
+    key = id.slice(0,3)
+    id_mapping.fetch(key, 'Generic')
   end
 end
